61db0c8e6ec7c7c9590300e0a47eb7545413bc55
xinkent <xinkent1994@gmail.com>
2020-06-20 13:55:48 +0900

    Fix bug

--- index.js
+++ index.js
@@ -29,24 +29,6 @@ app.post("/notes", (req, res) => {
     res.send(notes);
 });
 
-// app.put("/notes/:id", (req, res) => { // 1. データ(course)を探す let course = notes.find(e => e.id === parseInt(req.params.id)) if(!course){ res.send("該当idのコースが見つかりませんでした") } // 2. バリデーション let {error} = validate(req.body); if (error){ res.send(result.error.details[0].message); } // 3. データを編集し、結果を返す notes.forEach(e => { if (e.id === parseInt(req.params.id)) e.name = req.body.name; }); res.send(notes); }); app.get("/notes/:id", (req,res) =>{ let course = notes.find(e => e.id === parseInt(req.params.id))
-//     if(!course){
-//         res.send("該当idのコースが見つかりませんでした")
-//     }
-//     res.send(course);
-// });
-// 
-// app.get("/posts/:year/:month", (req, res) => {
-//     res.send(req.query);
-// });
-// 
-// app.listen(port, () =>{
-//     console.log(`ポート番号${port}で立ち上がりました。`)
-// });
-// 
-// function validate(course){
-//     const schema = {
-//         name: Joi.string().min(3).required()
-//     };
-//     return Joi.validate(course, schema);
-// }
+app.listen(port, () =>{
+    console.log(`ポート番号${port}で立ち上がりました。`)
+});
